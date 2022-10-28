ec74ae3b8b91e51576cc5b849d3ae07882fe17a4
Aiden Bostwick <aidenbostwick5@gmail.com>
2022-10-28 05:34:22 -0700

    unsure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import TodoList from './TodoList';
 
 function App() {
-  return null
+  return (
+    <>
+    <TodoList />
+    <input type="text"/>
+    <button> Add Todo</button>
+    <button>Clear compleated todo</button>
+    <div> 0 left todo</div>
+    </>
+  )
 }
 
 export default App;
